fd78051556b86a7fc62e49aefb782b54f52d50b7
bbipp <112016923+bbipp@users.noreply.github.com>
2023-05-08 22:36:30 -0400

    Update z.js

--- z.js
+++ z.js
@@ -1,12 +1,8 @@
-const zoomElement = document.querySelector(".zoom");
-let zoom = 1;
-const ZOOM_SPEED = 0.1;
-
-document.addEventListener("wheel", function(e) {  
-    
-    if(e.deltaY > 0){    
-        zoomElement.style.transform = `scale(${zoom += ZOOM_SPEED})`;  
-    }else{    
-        zoomElement.style.transform = `scale(${zoom -= ZOOM_SPEED})`;  }
-
-});
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
